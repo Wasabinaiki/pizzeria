7e6d8f1ec6c0ba4884eb12b4175667c4bab2da02
GRP SERRANO FONTAL RUBIANO TRUQUE SANUDO <wasabinaiki@gmail.com>
2024-10-21 22:44:43 -0500

    Migración Branches

---
+++ database/migrations/2024_10_22_034405_create_branches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->id(); // Clave primaria
+            $table->string('name', 255);
+            $table->string('address', 255);
+            $table->string('phone_number', 20)->nullable(); // Número de teléfono opcional
+            $table->timestamps(); // Campos created_at y updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches');
+    }
+};
