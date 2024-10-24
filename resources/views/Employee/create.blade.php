cf95bb36651c87cfe8ba1a1d833294f1bc15ca3c
Sebastian Sañudo <sebas2004lupe@gmail.com>
2024-10-23 19:11:07 -0500

    Se crea la vista de Ingredientes Extra

--- resources/views/Employee/create.blade.php
+++ resources/views/Employee/create.blade.php
@@ -1,17 +1,21 @@
-{{-- resources/views/pizza/create.blade.php --}}
+{{-- resources/views/extra_ingredient/create.blade.php --}}
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
-    <h1>Crear Nueva Pizza</h1>
-    <form action="{{ route('pizzas.store') }}" method="POST">
+    <h1>Crear Nuevo Ingrediente Extra</h1>
+    <form action="{{ route('extra_ingredients.store') }}" method="POST">
         @csrf
         <div class="form-group mb-3">
-            <label for="name">Nombre de la Pizza</label>
+            <label for="name">Nombre del Ingrediente</label>
             <input type="text" name="name" class="form-control" required>
         </div>
-        <button type="submit" class="btn btn-success">Crear Pizza</button>
-        <a href="{{ route('pizzas.index') }}" class="btn btn-secondary">Cancelar</a>
+        <div class="form-group mb-3">
+            <label for="price">Precio</label>
+            <input type="number" name="price" class="form-control" step="0.01" required>
+        </div>
+        <button type="submit" class="btn btn-success">Crear Ingrediente Extra</button>
+        <a href="{{ route('extra_ingredients.index') }}" class="btn btn-secondary">Cancelar</a>
     </form>
 </div>
 @endsection
