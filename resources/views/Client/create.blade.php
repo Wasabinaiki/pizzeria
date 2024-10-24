52d531f159839145eb9a662184aeb209d5116545
Sebastian Sañudo <sebas2004lupe@gmail.com>
2024-10-23 19:19:11 -0500

    Se crean vistas para Branches

--- resources/views/Client/create.blade.php
+++ resources/views/Client/create.blade.php
@@ -1,33 +1,21 @@
-{{-- resources/views/client/create.blade.php --}}
+{{-- resources/views/branch/create.blade.php --}}
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
-    <h1>Crear Nuevo Cliente</h1>
-    <form action="{{ route('clients.store') }}" method="POST">
+    <h1>Crear Nueva Sucursal</h1>
+    <form action="{{ route('branches.store') }}" method="POST">
         @csrf
         <div class="form-group mb-3">
-            <label for="name">Nombre</label>
+            <label for="name">Nombre de la Sucursal</label>
             <input type="text" name="name" class="form-control" required>
         </div>
         <div class="form-group mb-3">
-            <label for="email">Correo Electrónico</label>
-            <input type="email" name="email" class="form-control" required>
-        </div>
-        <div class="form-group mb-3">
-            <label for="password">Contraseña</label>
-            <input type="password" name="password" class="form-control" required>
-        </div>
-        <div class="form-group mb-3">
             <label for="address">Dirección</label>
-            <input type="text" name="address" class="form-control">
+            <input type="text" name="address" class="form-control" required>
         </div>
-        <div class="form-group mb-3">
-            <label for="phone">Teléfono</label>
-            <input type="text" name="phone" class="form-control">
-        </div>
-        <button type="submit" class="btn btn-success">Crear Cliente</button>
-        <a href="{{ route('clients.index') }}" class="btn btn-secondary">Cancelar</a>
+        <button type="submit" class="btn btn-success">Crear Sucursal</button>
+        <a href="{{ route('branches.index') }}" class="btn btn-secondary">Cancelar</a>
     </form>
 </div>
 @endsection
