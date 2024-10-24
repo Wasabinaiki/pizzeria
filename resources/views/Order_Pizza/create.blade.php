21d89905ec7f57149ac64565ad9e0665c4517a23
Sebastian Sañudo <sebas2004lupe@gmail.com>
2024-10-23 19:17:08 -0500

    Se creo la vista para Ordenar un Ingrediente Extra

--- resources/views/Order_Pizza/create.blade.php
+++ resources/views/Order_Pizza/create.blade.php
@@ -1,17 +1,25 @@
-{{-- resources/views/pizza/create.blade.php --}}
+{{-- resources/views/order_extra_ingredient/create.blade.php --}}
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
-    <h1>Crear Nueva Pizza</h1>
-    <form action="{{ route('pizzas.store') }}" method="POST">
+    <h1>Agregar Ingrediente Extra al Pedido</h1>
+    <form action="{{ route('order_extra_ingredient.store') }}" method="POST">
         @csrf
         <div class="form-group mb-3">
-            <label for="name">Nombre de la Pizza</label>
-            <input type="text" name="name" class="form-control" required>
+            <label for="order_id">ID del Pedido</label>
+            <input type="number" name="order_id" class="form-control" required>
         </div>
-        <button type="submit" class="btn btn-success">Crear Pizza</button>
-        <a href="{{ route('pizzas.index') }}" class="btn btn-secondary">Cancelar</a>
+        <div class="form-group mb-3">
+            <label for="extra_ingredient_id">ID del Ingrediente Extra</label>
+            <input type="number" name="extra_ingredient_id" class="form-control" required>
+        </div>
+        <div class="form-group mb-3">
+            <label for="quantity">Cantidad</label>
+            <input type="number" name="quantity" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-success">Agregar Ingrediente Extra</button>
+        <a href="{{ route('order_extra_ingredient.index') }}" class="btn btn-secondary">Cancelar</a>
     </form>
 </div>
 @endsection
